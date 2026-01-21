b692e4bf6c137a889a51eca6e1baa1499f42e504
Fernando Delmondes <fernandodelmondes40@gmail.com>
2026-01-21 12:28:09 -0300

    Adicionado a tela de conceito e feito alguns links entre paginas

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('galaxia_back/', views.galaxia_back_view, name='galaxia_back'),
     path('galaxia_front/', views.galaxia_front_view, name='galaxia_front'),
     path('planeta_construcao/', views.construcao_view, name='planeta_construcao'),
-    path('planeta_javascript/', views.planeta_javascript_view, name='planeta_javascript'),
+    path('planeta_javascript/', views.planeta_javascript_view, name='planeta_javascript_back'),
+
+    
+    
+    
+    path('conteudo/', views.conteudo_view, name='conteudo'),
 ]
--- app/views.py
+++ app/views.py
@@ -20,4 +20,7 @@ def construcao_view(request):
     return render(request, 'planeta_construcao.html')
 
 def planeta_javascript_view(request):
-    return render(request, 'planeta_javascript.html')
+    return render(request, 'planeta_javascript_back.html')
+
+def conteudo_view(request):
+    return render(request, 'conteudos/javascript_back/conceitos_da_linguagem.html')
