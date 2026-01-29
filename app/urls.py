f627afdca07fb57ebdb397d29f97b4646aceef6b
Fernando Delmondes <fernandodelmondes40@gmail.com>
2026-01-28 23:18:34 -0300

    Adicionado exercicios de teste

--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,28 @@ urlpatterns = [
     path('escolha_galaxia/', views.escolhaGalaxia_view, name='escolha_galaxia'),
     path('galaxia_back/', views.galaxia_back_view, name='galaxia_back'),
     path('galaxia_front/', views.galaxia_front_view, name='galaxia_front'),
+
+
+    #planetas front
+    path('planeta_html/', views.construcao_view, name='planeta_html'),
+    path('planeta_css/', views.construcao_view, name='planeta_css'),
+    path('planeta_javascript_web/', views.construcao_view, name='planeta_javascript_web'),
+    path('planeta_vue/', views.construcao_view, name='planeta_vue'),
+
+    #planetas back
     path('planeta_construcao/', views.construcao_view, name='planeta_construcao'),
     path('planeta_javascript/', views.planeta_javascript_view, name='planeta_javascript_back'),
 
     
     
-    
+    #conteudos
     path('conteudo/', views.conteudo_view, name='conteudo'),
+
+    #exercicios
+    path('exercicios_javascript_back/', views.exercicios_javascript_back_view, name='exercicios_javascript_back'),
+    path('exercicios_javascript_back_code/', views.exercicios_javascript_back_code_view, name='ex_conceitos_iniciais_code'),
+    path('acertar_resposta/', views.acerto_resposta_view, name='acertar_resposta'),
+    path('errar_resposta/', views.erro_resposta_view, name='errar_resposta'),
+    path('acertar_resposta_1/', views.acerto_resposta1_view, name='acertar_resposta_1'),
+    path('errar_resposta_1/', views.erro_resposta1_view, name='errar_resposta_1'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def login_view(request):
@@ -17,10 +17,49 @@ def galaxia_front_view(request):
     return render(request, 'galaxia_front.html')
 
 def construcao_view(request):
-    return render(request, 'planeta_construcao.html')
+    return render(request, 'planetas/planeta_construcao.html')
 
+#planetas
 def planeta_javascript_view(request):
     return render(request, 'planetas/planeta_javascript_back.html')
 
 def conteudo_view(request):
     return render(request, 'conteudos/javascript_back/conceitos_da_linguagem.html')
+
+
+
+
+
+#exercicios
+
+def acerto_resposta_view(request):
+    return render(request, 'exercicios/acerto_resposta.html')
+
+def erro_resposta_view(request):
+    return render(request, 'exercicios/erro_resposta.html')
+
+def acerto_resposta1_view(request):
+    return render(request, 'exercicios/acerto_resposta1.html')
+
+def erro_resposta1_view(request):
+    return render(request, 'exercicios/erro_resposta1.html')
+
+def exercicios_javascript_back_view(request):
+    if request.method == 'POST':
+       resposta = request.POST.get('opcao_escolhida')
+       if resposta == 'C':
+           return redirect('acertar_resposta')
+       elif resposta in ['A', 'B', 'D']:
+           return redirect('errar_resposta')
+    return render(request, 'exercicios/javascript_back/ex_conceitos_iniciais.html')
+
+def exercicios_javascript_back_code_view(request):
+    if request.method == 'POST':
+        # Aqui você pode processar os dados enviados pelo formulário
+        resposta = request.POST.get('opcao_escolhida')
+        if resposta == 'B':
+            return redirect('acertar_resposta_1')
+        elif resposta in ['A', 'C', 'D']:
+            return redirect('errar_resposta_1')
+        # Lógica para verificar a resposta e fornecer feedback pode ser adicionada aqui
+    return render(request, 'exercicios/javascript_back/ex_conceitos_iniciais_code.html')
